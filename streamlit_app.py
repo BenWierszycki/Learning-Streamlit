3935f1fc1653b27871ad8195fa0484b6a407007e
BenWierszycki <167548725+BenWierszycki@users.noreply.github.com>
2024-06-24 16:39:30 +0000

    learning Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,119 +1,65 @@
+
+
 import streamlit as st
 import pandas as pd
 
+st.title("This is My Special Testing APP!!!")
 
-st.title("📊 Data evaluation app")
+## session state allows us to keep variables - otherwise the script is reset and run from top to bottom each time
+## session state is a special dictionary for persistant variables
 
-st.write(
-    "We are so glad to see you here. ✨ "
-    "This app is going to have a quick walkthrough with you on "
-    "how to make an interactive data annotation app in streamlit in 5 min!"
-)
+if 'counter' not in st.session_state.keys():
+    st.session_state['counter'] = 0
 
-st.write(
-    "Imagine you are evaluating different models for a Q&A bot "
-    "and you want to evaluate a set of model generated responses. "
-    "You have collected some user data. "
-    "Here is a sample question and response set."
-)
+if st.button("Click me for balloons"):
+    st.balloons()
+    st.session_state['counter'] += 1
 
-data = {
-    "Questions": [
-        "Who invented the internet?",
-        "What causes the Northern Lights?",
-        "Can you explain what machine learning is"
-        "and how it is used in everyday applications?",
-        "How do penguins fly?",
-    ],
-    "Answers": [
-        "The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting"
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds.",
-    ],
-}
+if st.button("Click me for snow"):
+    st.snow()
+    st.session_state['counter'] += 1
 
-df = pd.DataFrame(data)
+st.write(f"You've hit a button {st.session_state['counter']} times")
 
-st.write(df)
+#################################################################################################
+#################################################################################################
 
-st.write(
-    "Now I want to evaluate the responses from my model. "
-    "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-    "You will now notice our dataframe is in the editing mode and try to "
-    "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇"
-)
+##  Playing with user inputs
 
-df["Issue"] = [True, True, True, False]
-df["Category"] = ["Accuracy", "Accuracy", "Completeness", ""]
+# we can control the title retrospectively
+my_title = st.empty()
 
-new_df = st.data_editor(
-    df,
-    column_config={
-        "Questions": st.column_config.TextColumn(width="medium", disabled=True),
-        "Answers": st.column_config.TextColumn(width="medium", disabled=True),
-        "Issue": st.column_config.CheckboxColumn("Mark as annotated?", default=False),
-        "Category": st.column_config.SelectboxColumn(
-            "Issue Category",
-            help="select the category",
-            options=["Accuracy", "Relevance", "Coherence", "Bias", "Completeness"],
-            required=False,
-        ),
-    },
-)
+user_choice = st.radio("Which is best?", options = ['Cats', 'Dogs'])
+st.write(f"You have chosen:  {user_choice}")
 
-st.write(
-    "You will notice that we changed our dataframe and added new data. "
-    "Now it is time to visualize what we have annotated!"
-)
+my_title.title(f"You've picked {user_choice}")
 
-st.divider()
+# -----------------------------------------------------------------------------
 
-st.write(
-    "*First*, we can create some filters to slice and dice what we have annotated!"
-)
+sweets = st.slider("Please pick best bumber of sweets to eat a day",
+        0, 100, step = 10)
 
-col1, col2 = st.columns([1, 1])
+if sweets <10:
+    st.write("Correct")
+elif sweets < 50:
+    st.write("Too many")
+else:
+    st.write("You need help")
+
+
+# --------------------------------------------------------------------------------------
+
+col1, col2, col3 = st.columns(3)
+col1.write("COL1")
+col2.write("COL2")
+col3.write("COL3")
+
 with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options=new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox(
-        "Choose a category",
-        options=new_df[new_df["Issue"] == issue_filter].Category.unique(),
-    )
+    col1.write("hello")
 
-st.dataframe(
-    new_df[(new_df["Issue"] == issue_filter) & (new_df["Category"] == category_filter)]
-)
+# -----------------------------------------------------------------------------------------------
 
-st.markdown("")
-st.write(
-    "*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`"
-)
 
-issue_cnt = len(new_df[new_df["Issue"] == True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
 
-col1, col2 = st.columns([1, 1])
-with col1:
-    st.metric("Number of responses", issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
 
-df_plot = new_df[new_df["Category"] != ""].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x="Category", y="count")
-
-st.write(
-    "Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:"
-)
 
